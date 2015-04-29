592e5ea170f345086541f9e18295f4886abb8f48
Oswald De Riemaecker <oswald@continuousphp.com>
2015-04-29 23:03:06 +0200

    Test

--- tests/ConnectorTest.php
+++ tests/ConnectorTest.php
@@ -6,13 +6,13 @@ class ConnectorTest extends \PHPUnit_Framework_TestCase {
 
 	public function testGeneralConnection() {
 		$connection = new Connector(new \MongoClient("mongodb"));
-		//$connection->setDb('test');
-		//$this->assertInstanceOf('Zumba\PHPUnit\Extensions\Mongo\Client\Connector', $connection);
-		//$connection->collection('test')->insert(array(
-		//	'document' => 'test document'
-		//));
-		//$this->assertCount(1, $connection->collection('test')->find());
-		//$connection->collection('test')->drop();
+		$connection->setDb('test');
+		$this->assertInstanceOf('Zumba\PHPUnit\Extensions\Mongo\Client\Connector', $connection);
+		$connection->collection('test')->insert(array(
+			'document' => 'test document'
+		));
+		$this->assertCount(1, $connection->collection('test')->find());
+		$connection->collection('test')->drop();
 	}
 
 }
