52ca30cc3fa5ecb72a504b783cc9f9cd8c569518
Oswald De Riemaecker <oswald@continuousphp.com>
2015-04-29 23:06:43 +0200

    Test

--- tests/ConnectorTest.php
+++ tests/ConnectorTest.php
@@ -5,7 +5,7 @@ use Zumba\PHPUnit\Extensions\Mongo\Client\Connector;
 class ConnectorTest extends \PHPUnit_Framework_TestCase {
 
 	public function testGeneralConnection() {
-		$connection = new Connector(new \MongoClient("mongodb"));
+		$connection = new Connector(new \MongoClient());
 		$connection->setDb('test');
 		$this->assertInstanceOf('Zumba\PHPUnit\Extensions\Mongo\Client\Connector', $connection);
 		$connection->collection('test')->insert(array(
