b104f9e7f21b255f721b742d45a1fce740957374
Oswald De Riemaecker <oswald@continuousphp.com>
2015-05-02 13:01:13 +0200

    Adding sleep

--- tests/ConnectorTest.php
+++ tests/ConnectorTest.php
@@ -6,7 +6,12 @@ class ConnectorTest extends \PHPUnit_Framework_TestCase {
 
 	public function testGeneralConnection() {
     //sleep(5);
-		$connection = new Connector(new \MongoClient());
+    $connection = new Connector(new \MongoClient(
+"mongodb://localhost:27017", 
+    array(
+        "socketTimeoutMS" => 100000
+    ) 
+));
 		$connection->setDb('test');
 		$this->assertInstanceOf('Zumba\PHPUnit\Extensions\Mongo\Client\Connector', $connection);
 		$connection->collection('test')->insert(array(
